ee433c772caad8c4d32af31e46060e8a45a5a026
Ricardo Micale <ricardomicale@gmail.com>
2021-08-18 22:23:45 -0400

    proyecto empezado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/button';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const numbers = [0,1,2,3,4,5,6,7,8,9]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Clicked { count } times!
+
+      <button onClick={() => setCount(count + 1)}>Click me!</button>
+      <button onClick={() => setCount(0)}>Reset count</button>
+      <button onClick={() => setCount(count * 10)}>Multiply count by 10</button>
+      <button onClick={() => setCount(count / 10)}>Divide count by 10</button>
+
+      <div>
+        {numbers.map(number => {
+          return (
+            <Button 
+            text={number} 
+            key={number} 
+            onClick={() => setCount(count + number)}
+            />
+          )
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Button(props) {
+
+    return (
+        <button>
+            <h3>{ props.text }</h3>
+        </button>
+    )
+}
