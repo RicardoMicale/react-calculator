7c7de4b6922f2346246052a377a9171d56370ab6
Ricardo Micale <ricardomicale@gmail.com>
2021-08-21 20:09:37 -0400

    signos agregados con funcion (revisar)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,85 @@
 import './App.css';
-import Button from './components/button';
 import { useState } from 'react';
 
 function App() {
+  //variables
   const [count, setCount] = useState(0);
+  const [resultado, setResultado] = useState(0);
+  const [signo, setSigno] = useState('')
   const [numero, setNumero] = useState('');
+  const signos = ['+', '-', '*', '/', '='];
   const numbers = [0,1,2,3,4,5,6,7,8,9];
+  let numerosUsados = []
+
+  //funciones
   const escribir = number => {
-    console.log(numero, number)
+    if(numero === '' && number === 0) {
+      return
+    }
     setNumero(numero + number);
   }
 
+  const operacion = (signoOp, numero) => {
+    const numeroInt = parseInt(numero)
+    setNumero('')
+    setSigno(signo)
+    numerosUsados.push(numeroInt);
+
+    numerosUsados.forEach(numero => {
+      if(signoOp === '+') {
+        setCount(count + numero)
+      } else if(signoOp === '-') {
+        setCount(count - numero)
+      } else if(signoOp === '*') {
+        setCount(count * numero)
+      } else if(signoOp === '/') {
+        setCount(count / numero)
+      } else if(signoOp === '=') {
+        setResultado(count)
+      } else {
+        return
+      }
+    });
+
+  }
+
+
+  //estructura
   return (
     <div className="App">
-      Clicked { count } times!
-
-      <button onClick={() => setCount(count + 1)}>Click me!</button>
-      <button onClick={() => setCount(0)}>Reset count</button>
-      <button onClick={() => setCount(count * 10)}>Multiply count by 10</button>
-      <button onClick={() => setCount(count / 10)}>Divide count by 10</button>
-
+      Resultado: { resultado }
       <div>
-        {numbers.map(number => {
+        {signos.map(signo => {
           return (
-            <Button 
-            text={number} 
-            key={number} 
-            onClick={() => escribir(number)}
-            />
+            <button
+            key={signo}
+            onClick={() => operacion(signo, numero)}
+            >
+              <h3>{ signo }</h3>
+            </button>
           )
         })}
       </div>
-      Escribiste { numero }
+      <div>
+      Numero: { numero !== '' ? numero : '---' }
+        {numbers.map(number => {
+          return (
+            <button
+            key={number} 
+            onClick={() => escribir(number)}
+            >
+              <h3>{ number }</h3>
+            </button>
+          )
+        })}
+        <button onClick={() => {
+          setNumero('');
+          setCount(0);
+          setResultado(0);
+          }}>
+          <h3>CLR</h3>
+        </button>
+      </div>
     </div>
   );
 }
